a1c84d61f72eb963708e76c34cc32d6d97497274
Rasmus Kaj <kaj@kth.se>
2012-09-13 22:15:44 +0200

    Nicer page classname.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require "wiki_format.php";
 ##
 
 $page = isset($_GET['page']) ? '/'.$_GET['page'] : '';
-$pageclass = 'page_' . preg_replace('/\W+/', '_', $page);
+$pageclass = 'stacken' . stringtolower(preg_replace('/\W+/', '_', $page));
 $url = "http://wiki.stacken.kth.se/wiki/Stacken{$page}?printable=yes";
 $content = @file_get_contents($url);
 
