34942b46f16ac514a6642b6cbbd5e210cb5b3c9e
Rasmus Kaj <kaj@kth.se>
2012-09-14 22:07:37 +0200

    Specify viewport.
    Specify that a small screen should render as a small screen, and
    not the bloody stupid default of a scaled down big screen that is
    the default on aparently all modern handhelds.

--- index.php
+++ index.php
@@ -95,6 +95,7 @@ ob_start();
 <html>
 	<head>
 		<title><?=$title?> - Datorföreningen Stacken</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<link type="text/css" rel="stylesheet" href="/style/style.css">
 	</head>
